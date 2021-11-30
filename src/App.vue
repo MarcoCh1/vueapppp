caef6b3c0f4cd99977a1bba6c74146c9406f4aa0
Marco Chico <mc0905091@otc.edu>
2021-11-29 22:48:33 -0600

    modified the gitignore file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Header theTitle="Vue To Due - Marco Chico"></Header>
+    <TaskList theTitle="My Tasks" v-bind:taskList="tasks" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import TaskList from "./components/TaskList.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    TaskList,
+  },
+
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: "Learn HTML & CSS", completed: false },
+        { id: 2, title: "Learn Javascript", completed: false },
+        { id: 3, title: "Learn a JS Framework", completed: false },
+        { id: 4, title: "Pass web Dev 2", completed: false },
+      ],
+    };
+  },
+  mounted() {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      this.tasks = res.data;
+    });
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,21 @@
+<!-- <TaskList> at src/components/TaskList.vue -->
+<template>
+  <div>
+    <h1>{{ theTitle }}</h1>
+    <p v-for="task in taskList" v-bind:key="task.id">
+      <Task v-bind:aTask="task" />
+    </p>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "TaskList",
+  props: ["theTitle", "taskList"],
+  components: {
+    Task,
+  },
+};
+</script>
